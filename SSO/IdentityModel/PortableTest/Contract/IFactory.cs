20709bbb4b8c81f62ee116d51805bdf46579d129
Stefan Hackauf <shackauf@saphir-software.de>
2015-12-15 07:37:36 +0100

    IdentityModel - Problem Portable

---
+++ SSO/IdentityModel/PortableTest/Contract/IFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using IdentityModel.Client;
+
+namespace Contract
+{
+    public interface IFactory
+    {
+        void Execute(Action<AuthorizeResponse> callbackFunction);
+    }
+}
