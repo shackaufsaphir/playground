79d880e6f313b84c83c18e10a3c977d2a94e50a7
Stefan Hackauf <shackauf@saphir-software.de>
2015-12-15 07:44:07 +0100

    IdentityModel - Problem Portable
    Example for IdentityModel - problem portable

---
+++ SSO/IdentityModel/PortableTest/Module/Module.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Contract;
+using IdentityModel.Client;
+
+namespace Module
+{
+    public class Module : IFactory
+    {
+        public void Execute(Action<AuthorizeResponse> callbackFunction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
